778186641eb73cebea534e4284715f7924999bb6
Miguel Peralta <eudysperalta723@gmail.com>
2020-02-09 12:24:13 -0800

    project init

---
+++ chatroom/src/components/chatForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ChatForm = () => {
+    return (
+        <div>
+            <form>
+                <textarea>
+                    
+                </textarea>
+            </form>
+        </div>
+    )
+}
+
+export default ChatForm;
