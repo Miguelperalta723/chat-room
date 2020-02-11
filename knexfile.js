4a0e2e177de8034aa46ef288d93f3dbfb8583834
Miguel Peralta <eudysperalta723@gmail.com>
2020-02-11 08:40:32 -0800

    added production env to knexfile

--- knexfile.js
+++ knexfile.js
@@ -12,5 +12,18 @@ module.exports = {
       directory: './data/migrations'
     }
   },
+   production: {
+    client: 'pg',    
+    connection: process.env.DATABASE_URL,
+    
+    
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+};
 
 };
