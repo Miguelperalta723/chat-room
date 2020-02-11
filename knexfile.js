50b5168465be6152c8ebb9057510ec3acc354cce
Miguel Peralta <eudysperalta723@gmail.com>
2020-02-11 13:56:08 -0800

    clean up

--- knexfile.js
+++ knexfile.js
@@ -16,14 +16,12 @@ module.exports = {
     client: 'pg',    
     connection: process.env.DATABASE_URL,
     
-    
     migrations: {
       directory: './data/migrations'
     },
     seeds: {
       directory: './data/seeds'
     }
-  },
-};
+  }
 
 };
