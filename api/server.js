ea767eb816d1cfa4971a5121f2b3867fe036cf67
Miguel Peralta <eudysperalta723@gmail.com>
2020-02-16 09:32:42 -0800

    added cors

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const dbConnection = require ('../data/dbConfig.js')
 const usersRouter = require('../users/users-router.js')
 const messagesRoutes = require('../logs/messages-router.js')
 const server = express()
 
+
 server.use(express.json())
+server.use(cors())
 server.use('/api/users', usersRouter)
 server.use('/api/messages', messagesRoutes )
 
