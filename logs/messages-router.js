fbff17b4dc8f42d8e1408dae598510635a9f9132
Miguel Peralta <eudysperalta723@gmail.com>
2020-02-18 09:41:34 -0800

    made routes public again

--- logs/messages-router.js
+++ logs/messages-router.js
@@ -3,7 +3,7 @@ const Logs = require('./messages-model')
 const restricted = require('./messages-middleware')
 const router = express.Router();
 
-router.get('/messages', restricted, (req, res) => {
+router.get('/messages', (req, res) => {
     Logs.find()
     .then(logs => {
         res.status(200).json({
@@ -17,7 +17,7 @@ router.get('/messages', restricted, (req, res) => {
     })
 });
 
-router.post('/add',restricted, (req, res) => {
+router.post('/add',  (req, res) => {
     const newMessage = req.body
     console.log(newMessage)
     Logs.insert(newMessage)
